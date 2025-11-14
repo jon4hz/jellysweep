0735ced467a508e8a7549848089170c7f52f546e
William Albertus Dembo <29192168+walbertus@users.noreply.github.com>
2025-11-14 14:18:26 +0700

    feat: add job to estimate media deletion date

---
+++ internal/engine/estimate.go
@@ -0,0 +1,34 @@
+package engine
+
+import (
+	"context"
+
+	"github.com/charmbracelet/log"
+)
+
+func (e *Engine) runEstimateDeletionsJob(ctx context.Context) error {
+	log.Info("starting estimate deletions job")
+
+	mediaItems, err := e.db.GetMediaItems(ctx, false)
+	if err != nil {
+		log.Error("failed to get media items for estimation", "error", err)
+		return err
+	}
+
+	for _, item := range mediaItems {
+		estimatedDeleteAt, err := e.policy.GetDeleteAt(ctx, item)
+		if err != nil {
+			log.Error("failed to estimate delete at for media item", "itemID", item.ID, "error", err)
+			continue
+		}
+		err = e.db.SetMediaEstimatedDeleteAt(ctx, item.ID, estimatedDeleteAt)
+		if err != nil {
+			log.Error("failed to set estimated delete at for media item", "itemID", item.ID, "error", err)
+			continue
+		}
+		log.Debug("estimated delete at set for media item", "itemID", item.ID, "estimatedDeleteAt", estimatedDeleteAt)
+	}
+
+	log.Info("estimate deletions job completed successfully")
+	return nil
+}
